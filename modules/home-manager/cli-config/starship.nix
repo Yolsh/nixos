4ba93b15e0eb8c85ea05f99c11046cc2104fa335
Yolsh <23JA1168@barton.ac.uk>
2024-11-01 01:32:58 +0000

    better prompt

--- modules/home-manager/cli-config/starship.nix
+++ modules/home-manager/cli-config/starship.nix
@@ -13,10 +13,11 @@
         format = "[ $time](fg:bold white bg:#9C466A)";
       };
       git_branch = {
-        format = "[ $symbol$branch(:$remote_branch)](fg:bold white bg:#7E3B73) ";
+        format = "[ $symbol$branch(:$remote_branch)](fg:bold white bg:#7E3B73)";
       };
       git_status = {
-        format = "([\[$all_status$ahead_behind\]](fg:bold white bg:#7E3B73) )";
+        format = "[\\[$all_status$ahead_behind\\] ]($style)";
+        style = "fg:bold white bg:#7E3B73";
       };
       os.disabled = false;
       directory = {
@@ -28,7 +29,7 @@
         success_symbol = "[](bold green)";
         error_symbol = "[](bold red)";
       };
-      format = "[](#E7896A)$username[](bg:#9C466A fg:#E7896A)$time[](bg:#7E3B73 fg:#9C466A)$git_branch$git_status[](bg:#563E75 fg:#7E3B73)[$golang$dotnet$python$rust](bg:#563E75)[](#563E75)\n$os$directory$character";
+      format = "[ ](bg:#1c1426)[ ](bg:#3e2831)[ ](bg:#603b3d)[ ](bg:#824f48)[ ](bg:#A36253)[ ](bg:#c5765f)[ ](bg:#E7896A)$username[](bg:#9C466A fg:#E7896A)$time[](bg:#7E3B73 fg:#9C466A)$git_branch$git_status[](bg:#563E75 fg:#7E3B73)[$golang$dotnet$python$rust](bg:#563E75)[](#563E75)\n$os$directory$character";
     };
   };
 }
