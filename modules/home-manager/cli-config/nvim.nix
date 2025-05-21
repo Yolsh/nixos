a82fd26c2cbf52ff9bcf713c03dbb813b12daa99
Yolsh <23JA1168@barton.ac.uk>
2025-05-21 18:29:51 +0100

    luas

--- modules/home-manager/cli-config/nvim.nix
+++ modules/home-manager/cli-config/nvim.nix
@@ -1,5 +1,53 @@
-{pkgs, ...}: {
-  programs.nvim = {
+{config, pkgs, ...}: {
+  programs.neovim = 
+    let
+      toLua = str: "lua << EOF\n${str}\nEOF\n";
+      toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+    in {
     enable = true;
-  }
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    plugins = with pkgs.vimPlugins; [
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/lsp.lua;
+      }
+
+      {
+        plugin = comment-nvim;
+        config = toLua "require('Comment').setup()";
+      }
+
+      {
+        plugin = gruvbox-nvim;
+        config = "colorscheme gruvbox";
+      }
+
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      {
+        plugin = (nvim-treesitter.withPlugins (p: [
+          p.tree-sitter-nix
+          p.tree-sitter-vim
+          p.tree-sitter-bash
+          p.tree-sitter-lua
+          p.tree-sitter-python
+          p.tree-sitter-json
+          p.tree-sitter-rust
+        ]));
+        config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
+    ];
+    extraLuaConfig = ''${builtins.readFile ./nvim/options.lua}'';
+  };
 }
---
+++ modules/home-manager/cli-config/nvim/options.lua
@@ -0,0 +1,13 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.number = true
+vim.o.relativenumber = true
+
+vim.o.signcolumn = "yes";
+
+vim.o.tabstop = 4
+vim.o.shiftwidth = 4
+
+vim.o.termguicolours = true
+vim.o.mouse = 'a'
