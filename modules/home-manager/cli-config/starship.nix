d82ae661bc1303402f1c81b59d846c031d2e297e
Yolsh <23JA1168@barton.ac.uk>
2025-05-22 16:49:51 +0100

    better starship

--- modules/home-manager/cli-config/starship.nix
+++ modules/home-manager/cli-config/starship.nix
@@ -4,21 +4,21 @@
     enableBashIntegration = true;
     settings = {
       add_newline = true;
-      pallete = "my_gruv";
+      palette = "my_gruv";
       palettes.my_gruv = {
-        grad_1 = '#282828';
-        grad_2 = '#292929';
-        grad_3 = '#463328';
-        grad_4 = '#623c26';
-        grad_5 = '#7e4523';
-        grad_6 = '#9c4e1f';
-        grad_7 = '#b95719';
-        fg0 = '#fbf1c7';
-        or_d = '#d65d0e';
-        or_l = '#fe8019';
-        purp_d = '#b16286';
-        purp_l = '#d3869b';
-      }
+        grad_1 = "#282828";
+        grad_2 = "#292929";
+        grad_3 = "#463328";
+        grad_4 = "#623c26";
+        grad_5 = "#7e4523";
+        grad_6 = "#9c4e1f";
+        grad_7 = "#b95719";
+        fg0 = "#fbf1c7";
+        or_d = "#d65d0e";
+        or_l = "#fe8019";
+        purp_d = "#b16286";
+        purp_l = "#d3869b";
+      };
       username = {
         show_always = true;
         style_user = "bg:or_d fg:fg0";
@@ -67,7 +67,7 @@
       rust = {
         style = "bg:purp_l fg:bold yellow";
       };
-      format = "[ ](bg:grad_1)[ ](bg:grad_2)[ ](bg:grad_3)[ ](bg:grad_4)[ ](bg:grad_5)[ ](bg:grad_6)[ ](bg:grad_7)$username[](bg:#9C466A fg:#E7896A)$time[](bg:#7E3B73 fg:#9C466A)$git_branch$git_status[](bg:#563E75 fg:#7E3B73)[$golang$dotnet$python$ocaml$zig](bg:#563E75)[](fg:#563E75)\n $nix_shell$directory$character";
+      format = " (bg:grad_1)[ ](bg:grad_2)[ ](bg:grad_3)[ ](bg:grad_4)[ ](bg:grad_5)[ ](bg:grad_6)[ ](bg:grad_7)$username[](bg:or_l fg:or_d)$time[](bg:purp_d fg:or_l)$git_branch$git_status[](bg:purp_l fg:purp_d)[$golang$dotnet$python$ocaml$zig](bg:purp_l)[](fg:purp_l)\n $nix_shell$directory$character";
     };
   };
 }
